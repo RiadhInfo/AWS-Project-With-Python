50bf5b6dbbb3d84d549082d93202d4b52455617c
MehdiHassayoun <tmf_concept@live.fr>
2023-01-14 20:11:26 +0100

    User

---
+++ src/user.py
@@ -0,0 +1,7 @@
+class User:
+def init(self, firstname, lastname, email, password):
+
+    self.FirstName = firstname
+    self.LastName = lastname
+    self.email = email
+    self.password = password 
